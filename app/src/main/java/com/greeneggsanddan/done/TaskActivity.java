5d1acf2ac764d753ab4ce657992a136a2ca8b579
greeneggsanddan <122955893+greeneggsanddan@users.noreply.github.com>
2023-03-05 16:19:23 -0800

    SwipeAdapter added

--- app/src/main/java/com/greeneggsanddan/done/TaskActivity.java
+++ app/src/main/java/com/greeneggsanddan/done/TaskActivity.java
@@ -4,11 +4,26 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.yalantis.library.Koloda;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class TaskActivity extends AppCompatActivity {
+    private SwipeAdapter adapter;
+    private List<Integer> list;
+    Koloda koloda;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_task);
+
+        koloda = findViewById(R.id.koloda);
+
+        list = new ArrayList<>();
+
+        adapter = new SwipeAdapter(this, list);
+        koloda.setAdapter(adapter);
     }
 }
