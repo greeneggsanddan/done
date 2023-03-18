b928054c6e6255a2b2b1ae94e85cd93a6591d370
greeneggsanddan <122955893+greeneggsanddan@users.noreply.github.com>
2023-03-17 20:18:38 -0700

    Fixed list elevation and animation

--- app/src/main/java/com/greeneggsanddan/done/RecyclerItemTouchHelper.java
+++ app/src/main/java/com/greeneggsanddan/done/RecyclerItemTouchHelper.java
@@ -94,8 +94,9 @@ public class RecyclerItemTouchHelper extends ItemTouchHelper.Callback {
             int iconRight = itemView.getLeft() + iconMargin + icon.getIntrinsicWidth();
             icon.setBounds(iconLeft, iconTop, iconRight, iconBottom);
 
-            background.setBounds(itemView.getLeft(), itemView.getTop(), itemView.getLeft() + ((int)dX) +backgroundCornerOffset, itemView.getBottom());
-        } else if (dX < 0) { // Swiping to the left //removed editItem functionality
+            background.setBounds(itemView.getLeft(), itemView.getTop(), itemView.getLeft()
+                    + ((int)dX) +backgroundCornerOffset, itemView.getBottom());
+        } else if (dX < 0) { // Swiping to the left
             int iconLeft = itemView.getRight() - iconMargin - icon.getIntrinsicWidth();
             int iconRight = itemView.getRight() - iconMargin;
             icon.setBounds(iconLeft, iconTop, iconRight, iconBottom);
