6caaa60712e671abbc4ef4c17fac8984a06fccc9
greeneggsanddan <122955893+greeneggsanddan@users.noreply.github.com>
2023-03-20 17:12:33 -0700

    Colors and themes updated. Dark mode disabled

--- app/src/main/java/com/greeneggsanddan/done/SplashActivity.java
+++ app/src/main/java/com/greeneggsanddan/done/SplashActivity.java
@@ -1,6 +1,7 @@
 package com.greeneggsanddan.done;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -13,6 +14,7 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
         getSupportActionBar().hide();
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO); //disables dark mode
 
         final Intent i = new Intent(SplashActivity.this, TaskActivity.class);
         new Handler().postDelayed(new Runnable() {
